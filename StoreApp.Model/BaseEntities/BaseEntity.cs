496d6d51278c343589bf010ce2c29c9f79e3ad10
GokhanKus <gkus1998@hotmail.com>
2024-01-07 15:03:07 +0300

    N tier mimarisi olusturuldu

---
+++ StoreApp.Model/BaseEntities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Model.BaseEntities
+{
+	public class BaseEntity : IEntity
+	{
+		public int Id { get; set; }
+		public DateTime CreatedTime { get; set; } = DateTime.Now;
+		public DateTime ModifiedTime { get; set; }
+	}
+}

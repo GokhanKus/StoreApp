cf88edbdb73a5b9f328cc19d9562b33576aeb8f3
GokhanKus <gkus1998@hotmail.com>
2024-01-12 21:28:43 +0300

    RepositoryManager

---
+++ StoreApp.DataAccess/ConcreteRepos/RepositoryManager.cs
@@ -0,0 +1,28 @@
+﻿using StoreApp.DataAccess.AbstractRepos;
+using StoreApp.DataAccess.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.DataAccess.ConcreteRepos
+{
+	public class RepositoryManager : IRepositoryManager
+	{
+		private readonly StoreContext _context;
+		private readonly IProductRepository _productRepository;
+		public RepositoryManager(IProductRepository productRepository, StoreContext context)
+		{
+			_context = context;
+			_productRepository = productRepository;
+		}
+
+		public IProductRepository Product => _productRepository;
+
+		public void Save()
+		{
+			_context.SaveChanges();
+		}
+	}
+}

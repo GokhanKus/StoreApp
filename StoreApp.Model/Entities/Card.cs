d9d0edf4d2f1c46f98c5257740e8837192fdce87
GokhanKus <gkus1998@hotmail.com>
2024-01-22 13:36:50 +0300

    Card.cs&CardLine.cs

---
+++ StoreApp.Model/Entities/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Model.Entities
+{
+	public class Card
+	{
+		public List<CardLine> CardLines { get; set; }
+		public Card()
+		{
+			CardLines = new List<CardLine>();
+		}
+		public void AddItem(Product product, int quantity)//metot imzası ilerde virtual olabilir, cunku bu metodu ezebiliriz.
+		{
+			CardLine? line = CardLines.Where(c => c.Product.Id == product.Id).FirstOrDefault();
+			if (line is null)
+			{
+				CardLines.Add(new CardLine
+				{
+					Product = product,
+					Quantity = quantity
+				});
+			}
+			else
+			{
+				line.Quantity += quantity;
+			}
+		}
+		public void RemoveItem(Product product) =>
+			CardLines.RemoveAll(c => c.Product.Id == product.Id);
+		public decimal ComputeTotalValue() =>
+			CardLines.Sum(i => i.Product.Price * i.Quantity);
+		public void Clear() =>
+			CardLines.Clear();
+	}
+}
---
+++ StoreApp.Model/Entities/CardLine.cs
@@ -0,0 +1,16 @@
+﻿using StoreApp.Model.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Model.Entities
+{
+	public class CardLine
+	{
+        public int CardLineId { get; set; }
+        public Product Product { get; set; } = new();
+        public int Quantity { get; set; }
+    }
+}

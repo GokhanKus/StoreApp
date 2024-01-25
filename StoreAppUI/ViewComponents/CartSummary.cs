9995668ae732926fe9ff8b37fea6d3d43c7fd20c
GokhanKus <gkus1998@hotmail.com>
2024-01-25 12:45:57 +0300

    CartSummaryWidget, CartSummaryViewComponent

---
+++ StoreAppUI/ViewComponents/CartSummary.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Model.Entities;
+
+namespace StoreAppUI.ViewComponents
+{
+	public class CartSummary:ViewComponent
+	{
+		private readonly Cart _cart;
+        public CartSummary(Cart cartService)
+        {
+            _cart = cartService;
+        }
+        public string Invoke()
+        {
+            return _cart.CartLines.Count.ToString();
+        }
+    }
+}

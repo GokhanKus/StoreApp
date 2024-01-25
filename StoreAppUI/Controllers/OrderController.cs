9f75d3e8e476d935a5010ae5954f761839d85359
GokhanKus <gkus1998@hotmail.com>
2024-01-25 17:36:18 +0300

    Complete Order(siparis olusturma sayfasi), OrderController.cs ve siparis icin alici bilgileri girme kısımlari

---
+++ StoreAppUI/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Business.AbstractServices;
+using StoreApp.Model.Entities;
+
+namespace StoreAppUI.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IServiceManager _manager;
+		private readonly Cart _cart;
+		public OrderController(IServiceManager manager, Cart cart)
+		{
+			_manager = manager;
+			_cart = cart;
+		}
+
+		public ViewResult CheckOut()
+		{
+			return View(new Order());
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult CheckOut([FromForm] Order order)
+		{
+			if (_cart.CartLines.Count() == 0)
+			{
+				ModelState.AddModelError("", "Sorry your cart is empty");
+			}
+			if (ModelState.IsValid)
+			{
+				order.Lines = _cart.CartLines.ToArray();
+				_manager.OrderService.SaveOrder(order);
+				_cart.Clear();
+				return RedirectToPage("/Complete",new {Id = order.Id});//.. numarali siparisiniz alinmistir tesekkur ederiz sayfasina gider.
+			}
+			return View();
+		}
+	}
+}

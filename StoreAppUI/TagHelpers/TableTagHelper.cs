76f6d67396bb6f46140e70e081e3e4496735bb86
GokhanKus <gkus1998@hotmail.com>
2024-01-30 11:30:03 +0300

    TableTagHelper.cs

---
+++ StoreAppUI/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreAppUI.TagHelpers
+{
+	[HtmlTargetElement("table")] //hedef alacagimiz html etiketi
+	//razor pagelerde kullanilan ve html etiketlerini genisletmek ve ilgili etiketlere cesitli islevsellikler kazandirmak uzere kullandigimiz bir yapi
+	public class TableTagHelper :TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.Attributes.SetAttribute("class","table table-hover table-bordered");
+		}
+	}
+}

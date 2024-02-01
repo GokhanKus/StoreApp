2169ece674b1e87c922e9f4b72678ce393c81941
GokhanKus <gkus1998@hotmail.com>
2024-02-01 14:13:32 +0300

    AccountController.cs, login islemi, LoginModel, Login.cshtml sayfasi

---
+++ StoreAppUI/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using StoreAppUI.IdentityModels;
+
+namespace StoreAppUI.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+		public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Login([FromForm] LoginModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				IdentityUser? user = await _userManager.FindByNameAsync(model.Name);
+				if (user != null)
+				{
+					await _signInManager.SignOutAsync(); //oturum acmis user var ise once oturumdan cikis islemini gerceklestirmeliyiz?
+					var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false); //giris yapma islemi
+					if (result.Succeeded)
+					{
+						return Redirect(model.ReturnUrl); //bizi "/"'ye yonlendirsin (homepage)
+					}
+				}
+				ModelState.AddModelError("Error", "Invalid Username or Password");
+			}
+			return View();
+		}
+	}
+}
---
+++ StoreAppUI/IdentityModels/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreAppUI.IdentityModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Name field is required")]
+        public string? Name{ get; set; }
+
+		[Required(ErrorMessage = "Password field is required")]
+		public string? Password{ get; set; }
+
+        private string? _returnUrl;
+        public string ReturnUrl //kapsulleme
+        {
+            get { return _returnUrl ?? "/"; }
+            set { _returnUrl = value; }
+        }
+    }
+}
